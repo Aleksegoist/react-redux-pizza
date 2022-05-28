3d3dcc12941b800a64c0a71edaf91e912e30b0e7
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-28 20:51:04 +0300

    add

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,48 +3,23 @@ import React, { useState } from 'react';
 const Categories = () => {
   const [indexCategory, setIndexCategory] = useState(0);
 
+  const category = ['All', 'Meat', 'Vegeterian', 'Cheese', 'Grill', 'Spicy'];
+
   const onClickCategory = (index) => {
     setIndexCategory(index);
   };
+
   return (
     <div className='categories'>
       <ul>
-        <li
-          onClick={() => onClickCategory(0)}
-          className={indexCategory === 0 ? 'active' : ''}
-        >
-          All
-        </li>
-        <li
-          onClick={() => onClickCategory(1)}
-          className={indexCategory === 1 ? 'active' : ''}
-        >
-          Meat
-        </li>
-        <li
-          onClick={() => onClickCategory(2)}
-          className={indexCategory === 2 ? 'active' : ''}
-        >
-          Vegeterian
-        </li>
-        <li
-          onClick={() => onClickCategory(3)}
-          className={indexCategory === 3 ? 'active' : ''}
-        >
-          Cheese
-        </li>
-        <li
-          onClick={() => onClickCategory(4)}
-          className={indexCategory === 4 ? 'active' : ''}
-        >
-          Grill
-        </li>
-        <li
-          onClick={() => onClickCategory(5)}
-          className={indexCategory === 5 ? 'active' : ''}
-        >
-          Spicy
-        </li>
+        {category.map((value, index) => (
+          <li
+            onClick={() => onClickCategory(index)}
+            className={indexCategory === index ? 'active' : ''}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
