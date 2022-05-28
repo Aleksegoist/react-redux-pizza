6457d335ffc8be0dd285382e5eed6bb1c98b346d
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-28 13:19:58 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './scss/app.scss';
 
 function App() {
-    return (
-        <div className='App'>
-            <h1>Hello</h1>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
