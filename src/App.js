002fdcb8116c9a86e8459df98952e4ca04112631
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-28 15:02:39 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Categories from './components/Categories';
 import Header from './components/Header';
+import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
@@ -12,6 +13,10 @@ function App() {
           <div className='content__top'>
             <Categories />
             <Sort />
+          </div>
+          <h2 className='content__title'>Все пиццы</h2>
+          <div className='content__items'>
+            <PizzaBlock />
           </div>
         </div>
       </div>
