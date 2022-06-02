918a1a67ce278ce9b66c6a9ff8a159f0fa1f638d
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-02 09:13:18 +0300

    add redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>
 );
 
