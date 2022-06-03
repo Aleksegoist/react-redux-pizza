b523609027a44fd512da5c07defee569120da835
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-03 12:50:05 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Cart from './pages/Cart';
 import PageNotFound from './pages/PageNotFound';
 import './scss/app.scss';
 import { createContext } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slices/filterSlice';
+
 
 
 export const SearchContext = createContext('')
@@ -15,25 +14,9 @@ export const SearchContext = createContext('')
 function App() {
   const [searchInput, setSearchInput] = useState('')
 
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
   return (
+
     <div className='wrapper'>
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(increment())}
-      >
-        Increment
-      </button>
-      <span>{count}</span>
-      <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        Decrement
-      </button>
-      {/* 
       <SearchContext.Provider value={{ searchInput, setSearchInput }}>
         <Header />
         <div className='content'>
@@ -43,8 +26,8 @@ function App() {
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
-      </SearchContext.Provider> */}
-    </div>
+      </SearchContext.Provider>
+    </div >
   );
 }
 
--- src/redux/slices/filterSlice.js
+++ src/redux/slices/filterSlice.js
@@ -1,25 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    value: 0,
+    categoryId: 0,
+    sort: {
+        name: 'popularity',
+        sortProperty: 'rating',
+    }
 }
 
-export const counterSlice = createSlice({
-    name: 'counter',
+const filterSlice = createSlice({
+    name: 'filter',
     initialState,
     reducers: {
-        increment: (state) => {
-
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action) => {
-            state.value += action.payload
-        },
-    },
+        setCategoryId(state, action) {
+            console.log(action)
+            state.categoryId = action.payload
+        }
+    }
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
-export default counterSlice.reducer
+export const { setCategoryId } = filterSlice.actions;
+
+export default filterSlice.reducer;
