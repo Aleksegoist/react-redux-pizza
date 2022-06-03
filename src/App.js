94cb436eff962a824cb22a9c14e748bb53acfa2e
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-03 12:24:23 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,14 +6,34 @@ import Cart from './pages/Cart';
 import PageNotFound from './pages/PageNotFound';
 import './scss/app.scss';
 import { createContext } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './redux/slices/filterSlice';
+
 
 export const SearchContext = createContext('')
 
 function App() {
   const [searchInput, setSearchInput] = useState('')
 
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
   return (
     <div className='wrapper'>
+      <button
+        aria-label="Increment value"
+        onClick={() => dispatch(increment())}
+      >
+        Increment
+      </button>
+      <span>{count}</span>
+      <button
+        aria-label="Decrement value"
+        onClick={() => dispatch(decrement())}
+      >
+        Decrement
+      </button>
+      {/* 
       <SearchContext.Provider value={{ searchInput, setSearchInput }}>
         <Header />
         <div className='content'>
@@ -23,7 +43,7 @@ function App() {
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
-      </SearchContext.Provider>
+      </SearchContext.Provider> */}
     </div>
   );
 }
